232da62de63820bb84edc359208099dd7664925f
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-19 19:13:05 +0545

    Task 4 completed

---
+++ Task 4/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Player
+    {
+        // Instance fields
+        public string playerName;
+        public int level;
+        public int health;
+
+        // Default constructor
+        public Player()
+        {
+            Console.WriteLine("Default constructor has been called");
+        }
+
+        // Parameterized constructor
+        public Player(string name, int lvl, int hp)
+        {
+            playerName = name;
+            level = lvl;
+            health = hp;
+        }
+    }
+}
--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Task_4
+﻿using System;
+namespace Task_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1. Using default constructor
+            Player p1 = new Player();
+            Console.WriteLine("Player 1 -> Name: " + p1.playerName + ", Level: " + p1.level + ", Health: " + p1.health);
+
+            // 2. Using parameterized constructor
+            Player p2 = new Player("Samir", 5, 100);
+            Console.WriteLine("Player 2 -> Name: " + p2.playerName + ", Level: " + p2.level + ", Health: " + p2.health);
+
+            Console.ReadLine();
         }
     }
 }
