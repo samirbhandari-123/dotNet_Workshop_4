7555bd1c3b0108c3db1829e252220b790c5b3708
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-19 11:56:13 +0545

    Task 1 Completed.

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Task_1
+﻿using System;
+
+namespace Task_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating first student object
+            Student s1 = new Student();
+            s1.name = "Samir";
+            s1.age = 21;
+            s1.faculty = "Computing";
+
+            // Creating second student object
+            Student s2 = new Student();
+            s2.name = "Bikash";
+            s2.age = 22;
+            s2.faculty = "Business";
+
+            // Display Student 1 details
+            Console.WriteLine("---- Student 1 ----");
+            Console.WriteLine("Name: " + s1.name);
+            Console.WriteLine("Age: " + s1.age);
+            Console.WriteLine("Faculty: " + s1.faculty);
+
+            // Display Student 2 details
+            Console.WriteLine("\n---- Student 2 ----");
+            Console.WriteLine("Name: " + s2.name);
+            Console.WriteLine("Age: " + s2.age);
+            Console.WriteLine("Faculty: " + s2.faculty);
+
+            // Display static field
+            Console.WriteLine("\nCollege Name (Static Field): " + Student.collegeName);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Task 1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Student
+    {
+        // Instance Fields
+        public string name;
+        public int age;
+        public string faculty;
+
+        // static field 
+        public static string collegeName = "Itahari International College";
+    }
+}
